a81682577814705cb9bb28e6523581158a7951d5
Josoa <josoarazafimandimby66@gmail.com>
2026-02-06 14:15:37 +0300

    Initial commit

---
+++ src/test/java/com/itu/frontOffice/FrontOfficeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itu.frontOffice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrontOfficeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
